40b2298218f44da97fb5bb26868a12a8fb11551a
murad <mouradhany574@gmail.com>
2024-05-08 00:04:32 +0300

    date done

--- app/src/main/java/com/example/lostandfound/ui/fragment/CreateAdvertFragment.java
+++ app/src/main/java/com/example/lostandfound/ui/fragment/CreateAdvertFragment.java
@@ -11,6 +11,7 @@ import androidx.navigation.NavController;
 import androidx.navigation.fragment.NavHostFragment;
 
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.RadioButton;
@@ -63,6 +64,16 @@ public class CreateAdvertFragment extends Fragment implements OnEntitySavedListe
                 }
                 CaseEntity entity = new CaseEntity(name,selectedRadioButtonText, phone, description, date, location);
                 mViewModel.saveEntity(entity);
+            }
+        });
+        binding.edTextDate.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    DatePickerFragment datePickerFragment = new DatePickerFragment(binding.edTextDate);
+                    datePickerFragment.show(requireActivity().getSupportFragmentManager(), "datePicker");
+                }
+                return true;
             }
         });
     }
